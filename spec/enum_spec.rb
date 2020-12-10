8196fbbf5b0a1aeb1f94378c7d35b9541ab25274
AbdelrhmanAmin <maxhd213@gmail.com>
2020-12-10 19:15:01 +0200

    Add testcases

--- spec/enum_spec.rb
+++ spec/enum_spec.rb
@@ -1,8 +1,6 @@
 
 require_relative "../lib/enum"
 
-  ## if(context) return it
-
 describe Enumerable do
   describe " \n #my_each \n" do 
     context "given an array \n" do
@@ -23,111 +21,111 @@ describe Enumerable do
       end
     end
   end
-  # describe "#my_select" do
-  #   context "select out friends who meet the condition" do
-  #     it "prints conditioned block then returns an empty array" do
-  #       expect(%w[Sharon Leo Leila Brian Arun].my_select { |friend| friend != 'Brian' }).to eql(["Sharon", "Leo", "Leila", "Arun"])
-  #     end
-  #   end
-  # end
+  describe "#my_select" do
+    context "select out friends who meet the condition" do
+      it "prints conditioned block then returns an empty array" do
+        expect(%w[Sharon Leo Leila Brian Arun].my_select { |friend| friend != 'Brian' }).to eql(["Sharon", "Leo", "Leila", "Arun"])
+      end
+    end
+  end
   
-  # describe "#my_all" do
-  #   context "print out true or false depending if all elements meet the condition" do
-  #     it "returns true" do
-  #       expect(%w[ant bear cat].my_all? { |word| word.length >= 3 }).to eql(true)
-  #     end
-  #     it "returns false" do
-  #       expect(%w[ant bear cat].my_all? { |word| word.length >= 4 }).to eql(false)
-  #     end
-  #     it "returns false" do
-  #       expect(%w[ant bear cat].my_all?(/t/) ).to eql(false)
-  #     end
-  #     it "returns true" do
-  #       expect([1, 2i, 3.14].my_all?(Numeric)).to eql(true)
-  #     end
-  #     it "returns true" do
-  #       expect([].my_all?).to eql(true)
-  #     end
-  #     it "returns false" do
-  #       expect([1, 2, 3].my_all?(3)).to eql(false)
-  #     end
-  #     it "returns true" do
-  #       expect([1, 2, 3].my_all? ).to eql(true)
-  #     end
-  #   end
-  # end
-  # describe "#my_any" do
-  #   context "print out friends" do
-  #     it "returns an empty array" do
-  #       expect(%w[ant bear cat].my_any? { |word| word.length >= 3 }).to eql(true)
-  #     end
-  #     it "returns an empty array" do
-  #       expect(%w[ant bear cat].my_any? { |word| word.length >= 4 }).to eql(true)
-  #     end
-  #     it "returns an empty array" do
-  #       expect(%w[ant bear cat].my_any?(/d/)).to eql(false)
-  #     end
-  #     it "returns an empty array" do
-  #       expect([nil, true, 99].my_any?(Integer)).to eql(true)
-  #     end
-  #     it "returns an empty array" do
-  #       expect([nil, true, 99].my_any?).to eql(true)
-  #     end
-  #     it "returns an empty array" do
-  #       expect([].my_any?).to eql(false)
-  #     end
-  #     it "returns an empty array" do
-  #       expect([1, 2, 3].my_any?(3)).to eql(true)
-  #     end
-  #     it "returns an empty array" do
-  #       expect(%w[e dd].my_any?(/d/)).to eql(true)
-  #     end
-  #     it "returns an empty array" do
-  #       expect(["car","cat","dog"].my_any?("cat")).to eql(true)
-  #     end
-  #   end
-  # end
-  # describe "#my_none" do
-  #   context "print out friends" do
-  #     it "returns an empty array" do
-  #       expect(%w[ant bear cat].my_none? { |word| word.length == 5 }).to eql(true)
-  #     end
-  #     it "returns an empty array" do
-  #       expect(%w[ant bear cat].my_none? { |word| word.length == 4 }).to eql(false)
-  #     end
-  #     it "returns an empty array" do
-  #       expect(%w[ant bear cat].my_none?(/d/)).to eql(true)
-  #     end
-  #     it "returns an empty array" do
-  #       expect([1, 3.14, 42].my_none?(Float)).to eql(false)
-  #     end
-  #     it "returns an empty array" do
-  #       expect([].my_none?).to eql(true)
-  #     end
-  #     it "returns an empty array" do
-  #       expect([nil].my_none?).to eql(true)
-  #     end
-  #     it "returns an empty array" do
-  #       expect([nil, false].my_none?).to eql(true)
-  #     end
-  #     it "returns an empty array" do
-  #       expect([nil, false, true].my_none?).to eql(false)
-  #     end
-  #     it "returns an empty array" do
-  #       expect(%w[e dd].my_none?(/d/)).to eql(false)
-  #     end
-  #     it "returns an empty array" do
-  #       expect([1, 2, 3].my_none?(3)).to eql(false)
-  #     end
-  #     it "returns an empty array" do
-  #       expect(["car","cat","dog"].my_none?("cat")).to eql(false)
-  #     end
-  #     it "returns an empty array" do
-  #       expect([1,2,2].my_none?(3)).to eql(true)
-  #     end
+  describe "#my_all" do
+    context "print out true or false depending if all elements meet the condition" do
+      it "returns true" do
+        expect(%w[ant bear cat].my_all? { |word| word.length >= 3 }).to eql(true)
+      end
+      it "returns false" do
+        expect(%w[ant bear cat].my_all? { |word| word.length >= 4 }).to eql(false)
+      end
+      it "returns false" do
+        expect(%w[ant bear cat].my_all?(/t/) ).to eql(false)
+      end
+      it "returns true" do
+        expect([1, 2i, 3.14].my_all?(Numeric)).to eql(true)
+      end
+      it "returns true" do
+        expect([].my_all?).to eql(true)
+      end
+      it "returns false" do
+        expect([1, 2, 3].my_all?(3)).to eql(false)
+      end
+      it "returns true" do
+        expect([1, 2, 3].my_all? ).to eql(true)
+      end
+    end
+  end
+  describe "#my_any" do
+    context "print out friends" do
+      it "returns an empty array" do
+        expect(%w[ant bear cat].my_any? { |word| word.length >= 3 }).to eql(true)
+      end
+      it "returns an empty array" do
+        expect(%w[ant bear cat].my_any? { |word| word.length >= 4 }).to eql(true)
+      end
+      it "returns an empty array" do
+        expect(%w[ant bear cat].my_any?(/d/)).to eql(false)
+      end
+      it "returns an empty array" do
+        expect([nil, true, 99].my_any?(Integer)).to eql(true)
+      end
+      it "returns an empty array" do
+        expect([nil, true, 99].my_any?).to eql(true)
+      end
+      it "returns an empty array" do
+        expect([].my_any?).to eql(false)
+      end
+      it "returns an empty array" do
+        expect([1, 2, 3].my_any?(3)).to eql(true)
+      end
+      it "returns an empty array" do
+        expect(%w[e dd].my_any?(/d/)).to eql(true)
+      end
+      it "returns an empty array" do
+        expect(["car","cat","dog"].my_any?("cat")).to eql(true)
+      end
+    end
+  end
+  describe "#my_none" do
+    context "print out friends" do
+      it "returns an empty array" do
+        expect(%w[ant bear cat].my_none? { |word| word.length == 5 }).to eql(true)
+      end
+      it "returns an empty array" do
+        expect(%w[ant bear cat].my_none? { |word| word.length == 4 }).to eql(false)
+      end
+      it "returns an empty array" do
+        expect(%w[ant bear cat].my_none?(/d/)).to eql(true)
+      end
+      it "returns an empty array" do
+        expect([1, 3.14, 42].my_none?(Float)).to eql(false)
+      end
+      it "returns an empty array" do
+        expect([].my_none?).to eql(true)
+      end
+      it "returns an empty array" do
+        expect([nil].my_none?).to eql(true)
+      end
+      it "returns an empty array" do
+        expect([nil, false].my_none?).to eql(true)
+      end
+      it "returns an empty array" do
+        expect([nil, false, true].my_none?).to eql(false)
+      end
+      it "returns an empty array" do
+        expect(%w[e dd].my_none?(/d/)).to eql(false)
+      end
+      it "returns an empty array" do
+        expect([1, 2, 3].my_none?(3)).to eql(false)
+      end
+      it "returns an empty array" do
+        expect(["car","cat","dog"].my_none?("cat")).to eql(false)
+      end
+      it "returns an empty array" do
+        expect([1,2,2].my_none?(3)).to eql(true)
+      end
       
-  #   end
-  # end
+    end
+  end
   # describe "#my_count" do
   #   context "print out friends" do
   #     it "returns an empty array" do
